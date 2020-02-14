939bdda9c51545b2819b51a9dc6ce8770b0dc6ae
Kringinal <53836468+Kringinal@users.noreply.github.com>
2020-02-14 03:19:00 -0500

    Update code.js

--- code.js
+++ code.js
@@ -9,6 +9,18 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 /* PACKAGES END */
 
+/* ADDONS */
+var Fortunes = {
+  "Yes",
+  "No",
+  "Fuck off",
+  "Not sure",
+  "Probably not loser",
+  "Go die"
+};
+
+/* END ADDONS */
+
 
 bot.on('ready', () => {
   console.log('Turned on Discord bot');
@@ -25,6 +37,14 @@ bot.on('message', async message => {
   
   else if (message.content.toLowerCase() === `${prefix}ping`)
     message.channel.send(`Pong!`)
+  
+  else if (message.content.toLowerCase() === `${prefix}8ball`)
+    if (args[1]) {
+      message.channel.send(Fortunes[math.floor(math.random() * Fortunes.length)]);
+    }
+  else {
+    message.channel.send(`Listen here fucker. I need a question so I can tell you if it will happen or not! :angry:`
+  }
 });
 
 
